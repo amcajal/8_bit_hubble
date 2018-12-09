21d8d85591867eea347cb7bdef3f298caac6c345
A. M. Cajal <amartin.glimpse23@gmail.com>
2018-12-09 23:30:06 +0100

    Update header and add user modifiable marks

--- project/dev/test/high_level_test/python_uat.py
+++ project/dev/test/high_level_test/python_uat.py
@@ -1,11 +1,44 @@
-#############################################################################
-# 8-bit Hubble User Aceptance Test (UAT)
+################################################################################
+#   Project: 8-Bit Hubble.
 #
-# The script launches in a command line interface (CLI) several commands
-# reproducing the user interaction with the application.
-# Not all possible combinations are tested; only those required to
-# demonstrate the proper behavior of the app.
-#############################################################################
+#   File: python_uat.py
+#
+#   Description: 
+#
+#       8-bit Hubble User Aceptance Test (UAT)
+#
+#       The script launches in a command line interface (CLI) several commands
+#       reproducing the user interaction with the application.
+#       Not all possible combinations are tested; only those required to
+#       demonstrate the proper behavior of the app.
+#
+#   Notes: The sections with the tag @USER_MODIFIABLE shall be modified by
+#           the user with the proper data of its local machine (i.e, paths
+#           to folders).
+#
+#   Contact: Alberto Martin Cajal, amartin.glimpse23<AT>gmail.com
+#
+#   URL: https://github.com/amcajal/8_bit_hubble
+#
+#   License: GNU GPL v3.0
+#
+#   Copyright (C) 2018 Alberto Martin Cajal
+#
+#   This file is part of 8-Bit Hubble.
+#
+#   8-Bit Hubble is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   8-Bit Hubble is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
 import sys
 import os
@@ -26,10 +59,14 @@ s_flag = "-s" # Flag to indicate the value of the seed
 h_flag = "-h" # Flag to print the help information
 v_flag = "-v" # Flag to print the version or "about" information
 
-o_dir = "./"
+
+# @USER_MODIFIABLE 
+o_dir = "./" 
 o_alt_dir = ".\\" # This reproduces a Windows directory, using a backslash as separator
 o_wrong_dir = "/this/do/not/exist"
 o_noperm_dir = "/path/to/no_perm/dir"
+# @USER_MODIFIABLE 
+
 
 name = "uat_image"
 name2 = "uat_image2"
